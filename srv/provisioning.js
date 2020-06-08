807e5ad3fa5c310feba055cca8d4ba1416a46964
Manu Gupta <manu.gupta01@sap.com>
2020-06-08 18:16:14 +0530

    fix for provision

--- srv/provisioning.js
+++ srv/provisioning.js
@@ -1,8 +1,22 @@
 module.exports = (service) => {
-    service.on('UPDATE', 'tenant', async (req, next) => {
-      const res = await next();     // first call default implementation which is doing the HDI container creation
-      let url = 'cf-isa-m-provider-provider-my-bookshop-srv.cfapps.eu10.hana.ondemand.com/Catalog';
-      console.log('[INFO ][ON_UPDATE_TENANT] ' + 'Application URL is ' + url);
-      return url;
-    } );
-  }
+  service.on('UPDATE', 'tenant', async (req, next) => {
+    const res = await next();     // first call default implementation which is doing the HDI container creation
+    let url = 'https://cf-isa-m-provider-provider-my-bookshop-srv.cfapps.eu10.hana.ondemand.com/admin';
+    console.log('[INFO ][ON_UPDATE_TENANT] ' + 'Application URL is ' + url);
+    return url;
+  } );
+  // event handler for returning the tenant specific application URL as a response to an onboarding request
+  // service.on('UPDATE', 'tenant', async (req, next) => {
+  //   const res = await next();          // IMPORTANT: call default implementation which is doing the HDI container creation
+  //   let c = cds.env.for('app');        // usemb cds config framework to read app specific config node
+  //   let appuri = typeof c.urlpart === "undefined" ? ' ' : c.urlpart;
+  //   if (appuri === ' ') {
+  //     console.log('[INFO ][ON_UPDATE_TENANT] Application URI for subscriptions is not configured.');
+  //     return '';
+  //   } else {
+  //     let url = 'https://' + req.data.subscribedSubdomain + appuri;
+  //     console.log('[INFO ][ON_UPDATE_TENANT] ' + 'Application URL is ' + url);
+  //     return url;
+  //   }
+  // });
+}
