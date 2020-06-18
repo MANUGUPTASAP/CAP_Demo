c2548b8e1630007ba855eea662b148e9db46be88
van den Hoek, Alex <a.van.den.hoek@sap.com>
2020-06-18 17:16:11 +0200

    updated files

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('express')();
- const cds = require('@sap/cds');
- cds.connect.to('db');
- cds.mtx.in(app);             // serve cds-mtx APIs
- cds.serve('all').in(app);   // serve business services
- const PORT = process.env.PORT || 4004;
- app.listen(PORT);
+const cds = require('@sap/cds');
+
+cds.connect.to('db');       // connect to multitenant datasource
+cds.mtx.in(app)            // serve cds-mtx APIs
+cds.serve('all').in(app); // serve all CAP business services
+
+const PORT = process.env.PORT || 4004;
+app.listen(PORT);
