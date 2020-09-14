40a75dad0d698c343f3dca3d5b528789467e4220
Manu <manu.gupta01@sap.com>
2020-09-14 14:25:18 +0000

    final change

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')();
-const cds = require('@sap/cds');
-
-cds.connect.to('db');       // connect to multitenant datasource
-cds.mtx.in(app)            // serve cds-mtx APIs
-cds.serve('all').in(app); // serve all CAP business services
-
-const PORT = process.env.PORT || 4004;
-app.listen(PORT);
